0d60da1532b01c4c547849d347555e51074e8c4f
Matt Smith <matt.smith3108@gmail.com>
2025-06-19 15:42:13 +0100

    Upgraded next version and changes globals from .scss to .css file

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '../globals.scss';
+import '../globals.css';
 import { getSiteInfo, getSiteInfoMeta } from '@/sanity/sanity-utils';
 import createMetadataObject from '../utils/createMetadataObject';
 
